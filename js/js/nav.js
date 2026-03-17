10f3556bca78f19cecbb6e6bb9c6961215664ea4
tozarslan-arch <tozarslan@gmail.com>
2026-03-17 12:59:54 -0500

    Delete js/js directory

--- js/js/nav.js
+++
@@ -1,9 +0,0 @@
-// Detect current page
-const page = document.body.getAttribute("data-page");
-
-// Highlight active tab
-document.querySelectorAll(".nav-item").forEach(item => {
-  if (item.dataset.page === page) {
-    item.classList.add("active");
-  }
-});
