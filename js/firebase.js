caf64ca614efb60623e6e4e702844c49a67193b6
tozarslan-arch <tozarslan@gmail.com>
2026-03-17 18:10:37 -0500

    Update firebase.js

--- js/firebase.js
+++ js/firebase.js
@@ -3,7 +3,7 @@ import { getAuth } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-aut
 import { getFirestore } from "https://www.gstatic.com/firebasejs/12.10.0/firebase-firestore.js";
 
 const firebaseConfig = {
-  appId: "1:1068914011666:web:90a7eeaee80e5b5063b2cb",
+  apiKey: "AIzaSyBPxT8nRj7eFwzpRwOYTJ_r_vAiT0u9Ssk",
   authDomain: "pill-time-tracker.firebaseapp.com",
   projectId: "pill-time-tracker",
   storageBucket: "pill-time-tracker.appspot.com",
@@ -11,6 +11,7 @@ const firebaseConfig = {
   appId: "1:1068914011666:web:90a7eeaee80e5b5063b2cb",
   measurementId: "G-TX72Q592LM"
 };
+
 
 const app = initializeApp(firebaseConfig);
 
